55f1547630ef255205e27db774115f2d16120f2c
Kristin Reikvam <kristin.reikvam@gjensidige.no>
2023-01-20 10:41:05 +0000

    macros and packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -5,7 +5,9 @@ select
     status,
 
     --- amount is stored in cents, convert it to dollars
-    amount /100 as amount,
+   ----- amount /100 as amount,    byttes ut med makro bare for trening, mindre lesbart
+        -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
     created as created_at  
 
 from {{ source('stripe', 'stripe_payments') }}
